cd514bd90d08b9d0f83dce3ae641cc06235afefc
David Isakson <ikeman32@yahoo.com>
2017-09-23 23:51:56 -0700

    Add files via upload

---
+++ envbuild.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+sudo apt install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 -y
+sudo apt install libboost-all-dev -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt update
+sudo apt install libdb4.8-dev libdb4.8++-dev -y
